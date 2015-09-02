5d3eff39d467f98f4128b3415e1714e99198a956
Kent C. Dodds <kent@doddsfamily.us>
2015-09-02 09:51:10 -0600

    Hiding token

--- webtask/index.js
+++ webtask/index.js
@@ -4,7 +4,10 @@ import rebaseAndMergeScript from './rebase-and-merge.sh';
 export default rebaseAndMerge;
 
 function rebaseAndMerge(context, cb) {
-  const command = exec(getScript(getPRData(context)), {}, (error, stdout, stderr) => {
+  exec(getScript(getPRData(context)), {}, (error, stdout, stderr) => {
+    stdout = hideToken(stdout || '');
+    stderr = hideToken(stderr || '');
+    error = cleanError(error);
     const response = !!error ? null : {type: 'Success', stdout, stderr};
     if (stderr) {
       console.error('stderr:', stderr);
@@ -33,7 +36,26 @@ function addAuthTokenToRepoUrl(token, url) {
 
 function getScript({baseRepo, baseBranch, prRepo, prBranch, dryRun}) {
   const args = [baseRepo, baseBranch, prRepo, prBranch, dryRun];
-  console.log('Calling Script with args: ', args.join(' ').replace(/https\:\/\/.*?@/g, 'https://token-hidden@'));
+  console.log('Calling Script with args: ', hideToken(args.join(' ')));
   return rebaseAndMergeScript.replace(/\$(\d)+?/g, (match, number) => `"${args[number - 1]}"`);
+}
+
+function cleanError(error) {
+  if (!error) {
+    return error;
+  }
+  if (typeof error === 'string') {
+    error = hideToken(error);
+  } else {
+    ['details', 'message', 'stack'].forEach(item => {
+      if (error[item]) {
+        error[item] = hideToken(error[item]);
+      }
+    });
+  }
+  return error;
+}
+function hideToken(string) {
+  return string.replace(/https\:\/\/.*?@/g, 'https://token-hidden@');
 }
 
